c00fa148fd0666df488fcd6e647e07dbcad0bf2c
WBlove <1248133194@qq.com>
2020-05-18 17:18:05 +0800

    dd

---
+++ bwcar/src/main/java/com/qf/excption/RException.java
@@ -0,0 +1,30 @@
+package com.qf.excption;
+
+public class RException extends RuntimeException {
+    private int code;
+    private String msg;
+
+    public RException() {
+        super();
+    }
+
+    public RException(String message) {
+        super(message);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
